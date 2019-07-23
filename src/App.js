6c283eaaa7fcaa1a7f4d05da9788b8bde947acaf
brandondorner <51007432+brandondorner@users.noreply.github.com>
2019-07-23 15:41:52 -0500

    Beginning Layout

--- src/App.js
+++ src/App.js
@@ -5,18 +5,23 @@ import Weather from './Weather'
 function App() {
 
   
-  const [results, setResults] = useState(['']);
+  const [results, setResults] = useState({});
   const [zip, setZip] = useState(['']);
   const [query, setQuery] = useState('70037')
   const [city, setCity] = useState('')
-  const [currentDate, setCurrentDate] = useState([''])
+
+  const [currentDate, setCurrentDate] = useState([''])  
   const [currentWeather, setCurrentWeather] = useState([])
-  const [day2, setDay2] = useState([])
+
+  const [day2, setDay2] = useState([''])
   const [day2Weather , setDay2Weather] = useState([])
+
   const [day3, setDay3] = useState([''])
   const [day3Weather , setDay3Weather] = useState([])
+
   const [day4, setDay4] = useState([''])
   const [day4Weather , setDay4Weather] = useState([])
+
   const [day5, setDay5] = useState([''])
   const [day5Weather , setDay5Weather] = useState([])
 
@@ -27,20 +32,25 @@ function App() {
   const retrieveResults = async () => {
     const response = await fetch (`http://api.openweathermap.org/data/2.5/forecast?zip=${query},us&APPID=f143ffd16d6f62d6220af4584c60c90a`)
     let data = await response.json()
-    setResults([data.list])
-    console.log(results)
+    setResults(data.list)
     setCity(data.city.name)
+
     setCurrentDate(data.list[0].main)
     setCurrentWeather(data.list[0].weather[0].description)
+
     setDay2(data.list[8].main)
     setDay2Weather(data.list[8].weather[0].description)
+
     setDay3(data.list[16].main)
     setDay3Weather(data.list[16].weather[0].description)
+
     setDay4(data.list[24].main)
     setDay4Weather(data.list[24].weather[0].description)
+
     setDay5(data.list[30].main)
     setDay5Weather(data.list[30].weather[0].description)
-    console.log(day2)
+    console.log(results)
+    console.log(data)
 
     
   }
@@ -64,7 +74,6 @@ function App() {
 
   return (
     <div className="App">
-      Hi
       <div className='local-weather'>
         Local Weather
         <div>
@@ -82,13 +91,14 @@ function App() {
       </form>
       <div className="results">
         <Weather
+          list = {results.cod}
           city={city}
-          currentDate={currentDate}
-          day2= {day2}
-          day3 = {day3}
-          day4 = {day4}
-          day5 = {day5}
           currentWeather = {currentWeather}
+          currentDate = {currentDate}
+          day2 = {day2}
+          day3 ={day3}
+          day4 ={day4}
+          day5 = {day5}
           day2Weather = {day2Weather}
           day3Weather = {day3Weather}
           day4Weather = {day4Weather}
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Weather.css'
 
 function Weather({city, currentWeather, day2Weather, day3Weather,
-    day4Weather, day5Weather, currentDate, day2, day3, day4, day5}){
+    day4Weather, day5Weather,currentDate, day2, day3, day4, day5}){
 
     return (
         <div className='weather'>
@@ -10,16 +11,72 @@ function Weather({city, currentWeather, day2Weather, day3Weather,
                  {city}
                 </div>
                 <div className='description'>
-                    {currentWeather}
+                    {/* Capitalizing the first letter of each word */}
+                    {(currentWeather.toString().split(' ').map( word => 
+                        word.charAt(0).toUpperCase() + word.slice(1)
+                        )).join(' ')}
                 </div>
                 <div className='current-temp'>
                  Current Temperature: {Math.round((currentDate.temp -273.15)* 9/5 + 32)}°
                 </div>
+                <div className='current-minmax'>
+                    {/* Unfortunately I cannot access the min/max temperatures from this
+                    API without purchasing a membership. So to emulate min/max(s) 
+                    I will just add a slight multiplier to the end of the formula. */}
+                    {Math.round(((currentDate.temp -273.15)* 9/5 + 32)*.9)}° - 
+                    {Math.round(((currentDate.temp -273.15)* 9/5 + 32)*1.1)}°
+                </div>
                 <div className='humidity'>
-                    Humidy: {currentDate.humidity}%
+                    Humidity: {currentDate.humidity}%
                 </div>
             </div>
             <div className='future'>
+                <div className='day2'>
+                    <div className='weather-description'>
+                        {/* Capitalizing the first letter of each word */}
+                        {(day2Weather.toString().split(' ').map( word => 
+                            word.charAt(0).toUpperCase() + word.slice(1)
+                            )).join(' ')}
+                    </div>
+                    <div className='minmax'>
+                        {Math.round(((day2.temp -273.15)* 9/5 + 32)*.90)}° - 
+                        {Math.round(((day2.temp -273.15)* 9/5 + 32)*1.1)}°
+                    </div>
+                </div>
+                <div className='day3'>
+                    <div className='weather-description'>
+                        {(day3Weather.toString().split(' ').map( word => 
+                            word.charAt(0).toUpperCase() + word.slice(1)
+                            )).join(' ')}
+                    </div>
+                    <div className='minmax'>
+                        {Math.round(((day3.temp -273.15)* 9/5 + 32)*.90)}° - 
+                        {Math.round(((day3.temp -273.15)* 9/5 + 32)*1.1)}°
+                    </div>
+                </div>
+                <div className='day4'>
+                    <div className='weather-description'>
+                        {(day4Weather.toString().split(' ').map( word => 
+                            word.charAt(0).toUpperCase() + word.slice(1)
+                            )).join(' ')}
+                    </div>
+                    <div className='minmax'>
+                        {Math.round(((day4.temp -273.15)* 9/5 + 32)*.90)}° - 
+                        {Math.round(((day4.temp -273.15)* 9/5 + 32)*1.1)}°
+                    </div>
+                </div>
+                <div className='day5'>
+                    <div className='weather-description'>
+                        {(day5Weather.toString().split(' ').map( word => 
+                            word.charAt(0).toUpperCase() + word.slice(1)
+                            )).join(' ')}
+                    </div>
+                    <div className='minmax'>
+                        {Math.round(((day5.temp -273.15)* 9/5 + 32)*.90)}° - 
+                        {Math.round(((day5.temp -273.15)* 9/5 + 32)*1.1)}°
+                    </div>
+                </div>
+
             </div>
         </div>
     )
